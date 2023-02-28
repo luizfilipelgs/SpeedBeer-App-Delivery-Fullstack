82f9aa8448460e3daecc57aa05927f367671e136
luizfilipelgs <luizfilipelgs@gmail.com>
2023-02-28 16:01:50 -0300

    chore: adiciona cors

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const loginRoute = require('../routes/login.route');
+const cors = require('cors');
 
 const app = express();
 
+app.use(express.json());
+app.use(cors());
+
 app.get('/coffee', (_req, res) => res.status(418).end());
+app.use('/login', loginRoute);
 
 module.exports = app;
