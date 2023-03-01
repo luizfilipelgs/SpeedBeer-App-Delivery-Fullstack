6be69203670c7bd7240ea8810eef417a43b9da1a
Malrus-dev <maelsobfer@gmail.com>
2023-03-01 16:50:56 -0300

    feat: backend, rota /products feita

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const loginRoute = require('../routes/login.route');
 const registerRoute = require('../routes/register.route');
+const productsRoute = require('../routes/products.route');
 
 const app = express();
 
@@ -11,5 +12,6 @@ app.use(cors());
 app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', loginRoute);
 app.use('/register', registerRoute);
+app.use('/products', productsRoute);
 
 module.exports = app;
