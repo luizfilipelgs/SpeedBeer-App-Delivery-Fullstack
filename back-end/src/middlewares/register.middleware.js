c95c81919f173ea04b67a45bf390460add99a8b8
LuisLaporta <lui.laporta99@gmail.com>
2023-03-01 04:49:21 -0300

    fix: diminui complexibilidade do middleware

--- back-end/src/middlewares/register.middleware.js
+++ back-end/src/middlewares/register.middleware.js
@@ -1,17 +1,13 @@
+const validName = (name) => name && name.length >= 12;
+
+const validPass = (password) => password && password.length >= 6;
+
+const validMail = (email) => email && /\S+@\S+\.\S+/.test(email);
+
 const validateRegister = (req, res, next) => {
   const { name, email, password } = req.body;
 
-  if (!name, !email || !password) {
-    return res
-      .status(400)
-      .json({ message: 'Todos os campos devem ser preenchidos' });
-  }
-
-  const isValidEmail = /\S+@\S+\.\S+/.test(email);
-
-  console.log(name.length < 12, name);
-
-  if (name.length < 12 || !isValidEmail || password.length < 6) {
+  if (!validName(name) || !validPass(password) || !validMail(email)) {
     return res
       .status(401)
       .json({ message: 'Formato de Email ou senha Invalidos' });
