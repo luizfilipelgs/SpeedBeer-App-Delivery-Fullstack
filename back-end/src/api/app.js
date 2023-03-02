339846d009d513f64ef95f803f5bbcf6a87e115a
LuisLaporta <lui.laporta99@gmail.com>
2023-03-02 11:55:31 -0300

    chore: rota de images e as imagens para o front

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -8,10 +8,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use('/images', express.static('public'));
 
-app.get('/coffee', (_req, res) => res.status(418).end());
 app.use('/login', loginRoute);
 app.use('/register', registerRoute);
 app.use('/products', productsRoute);
+app.get('/coffee', (_req, res) => res.status(418).end());
 
 module.exports = app;
