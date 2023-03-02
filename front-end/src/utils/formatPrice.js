6f0368427cfa73bbdac60076bda5a6650f3ffd8f
John Leonne <johnleonne@hotmail.com>
2023-03-02 15:49:18 -0400

    feat: utils formatPrice

---
+++ front-end/src/utils/formatPrice.js
@@ -0,0 +1,6 @@
+export default function formatPrice(price) {
+  const formattedValue = price
+    .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    .replace(/^(\D+)/, '$1 ');
+  return formattedValue;
+}
