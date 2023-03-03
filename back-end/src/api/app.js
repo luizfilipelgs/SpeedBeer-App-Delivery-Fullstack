c2d85e175f028e082074995c07892a68b21f7a49
Malrus-dev <maelsobfer@gmail.com>
2023-03-03 12:30:07 -0300

    fix: trocando a url de products

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -13,7 +13,7 @@ app.use('/images', express.static('public'));
 
 app.use('/login', loginRoute);
 app.use('/register', registerRoute);
-app.use('/customer/products', productsRoute);
+app.use('/products', productsRoute);
 app.use('/sale', saleRoute);
 app.get('/coffee', (_req, res) => res.status(418).end());
 
