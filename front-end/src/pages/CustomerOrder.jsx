0fd5665496b7837e06bce30e10746da48f3e5f80
John Leonne <johnleonne@hotmail.com>
2023-03-03 12:55:39 -0400

    fix: customerOder page adaptado para receber o mock

--- front-end/src/pages/CustomerOrder.jsx
+++ front-end/src/pages/CustomerOrder.jsx
@@ -1,38 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
 import OrderCard from '../components/OrderCard';
 import NavBar from '../components/navbar';
+import ListOrder from '../utils/Mock/ListOrder';
 
 function CustomerOrder() {
-  const [orders, setOrders] = useState([]);
+  // const [orders, setOrders] = useState([]);
 
-  useEffect(() => {
-    axios.get('http://localhost:3001/sales')
-      .then((response) => {
-        setOrders(response.data);
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
+  // useEffect(() => {
+  //   axios.get('http://localhost:3001/sales')
+  //     .then((response) => {
+  //       setOrders(response.data);
+  //       console.log(response.data);
+  //     })
+  //     .catch((error) => {
+  //       console.error(error);
+  //     });
+  // }, []);
 
   return (
     <div>
-      <header>
-        <p>CustomerOrder</p>
-      </header>
+
       <NavBar />
 
       <div>
-        <h2>Pedidos</h2>
-        { orders.map((order) => (
+
+        { ListOrder.map((order) => (
           <OrderCard
             key={ order.id }
             id={ order.id }
             status={ order.status }
             date={ new Date((order.saleDate)) }
-            Price={ order.totalPrice }
+            totalPrice={ order.totalPrice }
           />
         )) }
       </div>
