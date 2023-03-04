5fd82dfe0f7b4b4248ba7c813503e093504705b4
John Leonne <johnleonne@hotmail.com>
2023-03-04 10:06:31 -0400

    fix: adiciona condicional em formatPrice

--- front-end/src/utils/formatPrice.js
+++ front-end/src/utils/formatPrice.js
@@ -1,6 +1,6 @@
 export default function formatPrice(price) {
   const formattedValue = price
-    .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
+    ?.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })
     .replace(/^(\D+)/, '$1 ');
   return formattedValue;
 }
