058e89e90a1d65dcbbd08d29ead74303aeaf1820
John Leonne <johnleonne@hotmail.com>
2023-03-04 20:00:18 -0400

    style: adiciona um css provisório no navbar para melhorar a visualização do componente.

--- front-end/src/components/navbar.jsx
+++ front-end/src/components/navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate, NavLink } from 'react-router-dom';
 import { getLocalStorage, delLocalStorage } from '../services/localStorage';
+import '../App.css';
 
 const ROUTE = 'customer_products';
 const PRODUCTS = 'element-navbar-link-products';
@@ -18,38 +19,112 @@ function NavBar() {
   };
 
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink
-            data-testid={ `${ROUTE}__${PRODUCTS}` }
-            to="/customer/products"
-          >
-            Produtos
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            data-testid={ `${ROUTE}__${ORDERS}` }
-            to="/customer/orders"
-          >
-            Pedidos
-          </NavLink>
-        </li>
-        <li
-          data-testid={ `${ROUTE}__${FULL_NAME}` }
+    <nav
+      style={ {
+        borderBottom: '1px solid black',
+        paddingBottom: '18.5px',
+        marginBottom: '24px',
+      } }
+    >
+      <ul
+        style={ {
+          display: 'flex',
+          listStyle: 'none',
+        } }
+      >
+        <div
+          style={ {
+            display: 'flex',
+            textAlign: 'center',
+            justifyContent: 'space-around',
+            minWidth: '300px',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+          } }
         >
-          { user.name }
-        </li>
-        <li>
-          <button
-            type="button"
-            data-testid={ `${ROUTE}__${LOGOUT}` }
-            onClick={ handleLogout }
+          <li>
+            <NavLink
+              to="/customer/products"
+              activeclassname="active"
+              className="nav-link"
+              data-testid={ `${ROUTE}__${PRODUCTS}` }
+              style={ {
+                display: 'flex',
+                padding: '16px',
+                justifyContent: 'center',
+                minWidth: '150px',
+                textDecoration: 'none',
+              } }
+            >
+              Produtos
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/customer/orders"
+              activeclassname="active"
+              className="nav-link"
+              data-testid={ `${ROUTE}__${ORDERS}` }
+              style={ {
+                display: 'flex',
+                padding: '16px',
+                justifyContent: 'center',
+                minWidth: '150px',
+                textDecoration: 'none',
+              } }
+            >
+              Pedidos
+            </NavLink>
+          </li>
+        </div>
+
+        <div
+          style={ {
+            display: 'flex',
+            textDecoration: 'none',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+            minWidth: '300px',
+            position: 'absolute',
+            top: 0,
+            right: 0,
+          } }
+        >
+          <li
+            data-testid={ `${ROUTE}__${FULL_NAME}` }
+            style={ {
+              display: 'flex',
+              padding: '16px',
+              justifyContent: 'center',
+              minWidth: '150px',
+              textDecoration: 'none',
+              color: '#000',
+            } }
           >
-            Sair
-          </button>
-        </li>
+            {user.name}
+          </li>
+          <li>
+            <button
+              type="button"
+              data-testid={ `${ROUTE}__${LOGOUT}` }
+              onClick={ handleLogout }
+              style={ {
+                display: 'flex',
+                margin: '0 24px',
+                padding: '8px',
+                cursor: 'pointer',
+                justifyContent: 'center',
+                alignItems: 'center',
+                minWidth: '100px',
+                textDecoration: 'none',
+                color: '#000',
+              } }
+            >
+              Sair
+            </button>
+          </li>
+        </div>
       </ul>
     </nav>
   );
