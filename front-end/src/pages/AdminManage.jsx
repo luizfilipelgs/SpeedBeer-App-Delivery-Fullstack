39451abea0c5bf85b33508c680fe8d993c4b8dfb
John Leonne <johnleonne@hotmail.com>
2023-03-08 16:24:45 -0400

    feat: requisição para buscar os usuários

--- front-end/src/pages/AdminManage.jsx
+++ front-end/src/pages/AdminManage.jsx
@@ -1,6 +1,31 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { getLocalStorage } from '../services/localStorage';
 
 function AdminManage() {
+  const [users, setUsers] = useState([]);
+  const user = getLocalStorage('user');
+
+  const getAllUser = async () => {
+    try {
+      const response = await axios.get('http://localhost:3001/login/users');
+
+      const dataFiltered = response.data.filter((d) => d.id !== user.id);
+      console.log(dataFiltered, 'dataFiltered');
+      if (dataFiltered) {
+        setUsers(dataFiltered);
+      } else {
+        console.log('Ocorreu um erro');
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getAllUser();
+  }, []);
+
   return (
 
     <div>
