087e769761bbe3f6487cd803e873a683857a1390
John Leonne <johnleonne@hotmail.com>
2023-03-09 02:30:10 -0400

    refactor: AdminManage page

--- front-end/src/pages/AdminManage.jsx
+++ front-end/src/pages/AdminManage.jsx
@@ -1,44 +1,38 @@
 import axios from 'axios';
-import React, { useContext, useEffect, useState } from 'react';
-import LoginContext from '../context/LoginContext';
+import React, { useEffect, useState } from 'react';
 import FormRegister from '../components/FormRegister';
 import NavBar from '../components/Navbar';
 import { getLocalStorage } from '../services/localStorage';
 import { STATUS_CODE_CONFLICT } from '../utils/Types';
+import UsersTable from '../components/UsersTable';
 
 function AdminManage() {
-  const { setUser } = useContext(LoginContext);
+  const [users, setUsers] = useState([]);
   const [registerError, setRegisterError] = useState('');
   const user = getLocalStorage('user');
 
   const getAllUser = async () => {
     try {
-      const response = await axios.get(
-        'http://localhost:3001/login/users',
-
-        {
-          headers: {
-            Authorization: user.token,
-          },
+      const response = await axios.get('http://localhost:3001/login/users', {
+        headers: {
+          Authorization: user.token,
         },
-      );
+      });
 
-      const dataFiltered = response.data.filter((d) => d.id !== user.id);
-
-      if (dataFiltered) {
-        setUser(dataFiltered);
-      } else {
-        console.log('Ocorreu um erro');
-      }
+      setUsers(response.data.filter((d) => d.id !== user.id));
     } catch (error) {
       console.log(error);
     }
   };
 
+  useEffect(() => {
+    getAllUser();
+  }, []);
+
   const handleSubmit = async (e, { name, email, password, role }) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post(
+      await axios.post(
         'http://localhost:3001/register/admin',
         { name, email, password, role },
         {
@@ -48,8 +42,7 @@ function AdminManage() {
         },
       );
 
-      setUser(data);
-      getAllUser();
+      await getAllUser();
       setRegisterError('');
     } catch (error) {
       if (error.response && error.response.status === STATUS_CODE_CONFLICT) {
@@ -60,14 +53,11 @@ function AdminManage() {
     }
   };
 
-  useEffect(() => {
-    getAllUser();
-  }, []);
-
   return (
     <div>
       <NavBar />
       <FormRegister handleSubmit={ handleSubmit } registerError={ registerError } />
+      <UsersTable users={ users } />
     </div>
   );
 }
