a6df2baef939cd469b887dad17af4e3cbd26f7af
luizfilipelgs <luizfilipelgs@gmail.com>
2023-03-17 19:40:03 -0300

    fix: renomeando arquivo page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginProvider from './context/LoginProvider';
-import Login from './pages/FormLogin';
+import Login from './pages/login';
 import Register from './pages/Register';
 import Products from './pages/Products';
 import CustomerOrder from './pages/CustomerOrder';
